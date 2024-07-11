82d938b97da27ff7fd65dd59d3a01238647fe9b8
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-07-11 16:58:38 +0300

    Css

--- CourseProject/src/Course.jsx
+++ CourseProject/src/Course.jsx
@@ -1,27 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-
 function Course({ image, title, description }) {
-  // console.log(props);
-  // const title = props.title;
-  // const description = props.description;
-  // const { title, description } = props;
-
   return (
-    <div>
-      <img src={image} alt="" />
-      <div>{title}</div>
-      <div>{description}</div>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-4by3">
+          <img src={image} alt="Kurslarim" />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media">
+          <div className="media-content">
+            <p className="title is-4">{title}</p>
+          </div>
+        </div>
+        <div className="content">
+          {description}
+          <br />
+          <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+        </div>
+      </div>
     </div>
   );
 }
 
 Course.propTypes = {
+  image: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-
 };
 
-export default Course;  
+export default Course;
